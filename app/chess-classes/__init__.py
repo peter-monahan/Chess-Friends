249f14be1da3924266a01e6550133378f113320b
peter <peter.b.monahan@gmail.com>
2022-11-15 17:17:19 -0600

    just getting started

---
+++ app/chess-classes/__init__.py
@@ -0,0 +1,18 @@
+
+
+class Piece:
+  def __init__(self, color, start_coords, curr_coords, times_moved):
+    self.color = color
+    self.start_coords = start_coords
+    self.curr_coords = curr_coords
+    self.times_moved = times_moved
+    self.valid_moves = []
+
+
+  def get_line_of_sight(self):
+    res = []
+
+
+
+  def get_valid_moves(self):
+    res = []
