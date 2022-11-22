0715376e2d8d33edb074d8a7b39fc3a38424a5ef
peter <peter.b.monahan@gmail.com>
2022-11-21 22:01:04 -0600

    more work done on the routes and store

--- app/api/friend_routes.py
+++ app/api/friend_routes.py
@@ -3,6 +3,7 @@ from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
 from app.models import User, Friend_Request, db
 from datetime import datetime
+from app.sockets import socketio
 
 friend_routes = Blueprint('friends', __name__)
 
@@ -31,9 +32,12 @@ def delete_friendship(id):
     if friend in user.friends:
       user.unfriend(friend)
       db.session.commit()
-      return {'message': 'Success'}
+      if friend.session_id:
+        socketio.emit('delete_friend', user.to_dict(),
+                      room=friend.session_id)
+      return {'message': 'Success', 'item': friend.to_dict()}
     else:
-      return {'errors': ['User must be a friend to be unfriended']}
+      return {'errors': ['User must be a friend to be unfriended']}, 400
   else:
     return jsonify({'message': 'User could not be found'}), 404
 
@@ -44,10 +48,13 @@ def delete_friendship(id):
 def create_friend_request():
   form = FriendRequestForm()
   if form.validate_on_submit:
+    receiver = User.query.get(form.data['receiver_id']) # ALERT MAY NEED TO BE INT WATCHOUT FOR BUGS
     friend_request = Friend_Request(sender_id=current_user.id, receiver_id=form.data['receiver_id'])
     db.session.add(friend_request)
     db.session.commit()
-
+    if receiver.session_id:
+      socketio.emit('new_friend_request', friend_request.to_dict(),
+                  room=receiver.session_id)
     return friend_request.to_dict()
   else:
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
@@ -72,15 +79,15 @@ def accept_friend_request(id):
 
   if request:
     if request.receiver_id == current_user.id:
-      # user = User.query.get(current_user.id)
-      # friend = User.query.get(request.sender_id)
-      # user.befriend(friend)
       request.receiver.befriend(request.sender)
       db.session.delete(request)
       db.session.commit()
-      return {'message': 'Success'}
+      if request.sender.session_id:
+        socketio.emit('new_friend', request.receiver.to_dict(),
+                      room=request.sender.session_id)
+      return request.sender.to_dict()
     else:
-      return {'errors': ['Unauthorized']}
+      return {'errors': ['Unauthorized']}, 401
   else:
     return jsonify({'message': 'Friend request could not be found'}), 404
 
@@ -92,11 +99,21 @@ def delete_friend_request(id):
   request = Friend_Request.query.get(id)
 
   if request:
-    if request.receiver_id == current_user.id or request.sender_id == current_user.id:
+    if request.receiver_id == current_user.id:
+      other_user = User.query.get(request.sender_id)
       db.session.delete(request)
       db.session.commit()
-      return {'message': 'Success'}
+      if other_user.session_id:
+        socketio.emit('delete_friend_request', request.to_dict())
+      return {'message': 'Success', 'item': request.to_dict()}
+    elif request.sender_id == current_user.id:
+      other_user = User.query.get(request.receiver_id)
+      db.session.delete(request)
+      db.session.commit()
+      if other_user.session_id:
+        socketio.emit('delete_friend_request', request.to_dict())
+      return {'message': 'Success', 'item': request.to_dict()}
     else:
-      return {'errors': ['Unauthorized']}
+      return {'errors': ['Unauthorized']}, 401
   else:
     return jsonify({'message': 'Friend request could not be found'}), 404
---
+++ react-app/src/store/friendRequests.js
@@ -0,0 +1,96 @@
+//all actions specific to COMMENTS Resource
+
+//imports
+import { myFetch } from "./myFetch";
+
+//constants
+const GET_ALL_FRIEND_REQUESTS = 'GET_ALL_FRIEND_REQUESTS';
+const CREATE_FRIEND_REQUEST = 'CREATE_FRIEND_REQUEST';
+const DELETE_FRIEND_REQUEST = 'DELETE_FRIEND_REQUEST';
+
+
+//ACTION CREATORS
+const getFriendRequests = (friendRequests) => {
+  return {
+    type: GET_ALL_FRIEND_REQUESTS,
+    friendRequests
+  }
+};
+
+
+export const addFriendRequest = (friendRequest) => {
+  return {
+    type: CREATE_FRIEND_REQUEST,
+    friendRequest,
+  }
+};
+
+
+const deleteFriendRequest = (friendRequestId) => {
+  return {
+    type: DELETE_FRIEND_REQUEST,
+    friendRequestId
+  }
+};
+
+//Thunks
+
+//GET ALL Comments
+export const getAllFriendRequests = () => async (dispatch) => {
+  const res = await myFetch(`/api/friends/requests`);
+  if (res.ok) {
+    const friendRequests = await res.json();
+    dispatch(getFriendRequests(friendRequests));
+  }
+  return res;
+};
+
+//CREATE Comment
+export const createFriendRequest = (receiver_id) => async (dispatch) => {
+
+  const res = await myFetch(`/api/friends/requests`, {
+    method: 'POST',
+    body: JSON.stringify({
+      receiver_id
+    })
+  });
+
+  if (res.ok) {
+    const newFriendRequest = await res.json();
+    dispatch(addFriendRequest(newFriendRequest));
+    return res
+  }
+};
+
+
+//DELETE Comment
+export const deleteAFriendRequest = (friendRequestId) => async (dispatch) => {
+  const res = await myFetch(`/api/friends/requests/${friendRequestId}`, {
+    method: 'DELETE'
+  });
+  const response = await res.json();
+  if (res.ok) {
+    dispatch(deleteFriendRequest(friendRequestId));
+  }
+  return response;
+};
+
+
+const initialState = {};
+
+//Comments REDUCER
+export default function reducer(state = initialState, action) {
+  let newState = { ...state }
+  switch (action.type) {
+    case GET_ALL_FRIEND_REQUESTS:
+      return action.friendRequests
+    case CREATE_FRIEND_REQUEST:
+      newState[action.friendRequest.id] = action.friendRequest
+      return newState;
+    case DELETE_FRIEND_REQUEST:
+      delete newState[action.friendRequestId]
+      return newState;
+    default:
+      return state;
+  };
+};
