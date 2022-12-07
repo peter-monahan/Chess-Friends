e0bfbaa72a7fb22e320535665fdb5ecd97e47832
peter <peter.b.monahan@gmail.com>
2022-12-07 05:54:02 -0600

    live site troubleshooting undo

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -5,7 +5,7 @@ from app.models import User
 user_routes = Blueprint('users', __name__)
 
 
-@user_routes.route('/')
+@user_routes.route('')
 def users():
     users = User.query.all()
     return {user.id: user.to_dict() for user in users}
